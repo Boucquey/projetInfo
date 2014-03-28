4710f54f9d33e1de7043030b70064a233620c174
Boucquey <boucqueyn@gmail.Com>
2014-03-28 08:35:52 +0100

    Débuts

--- projetInfo/projetInfo/Form1.Designer.cs
+++ projetInfo/projetInfo/Form1.Designer.cs
@@ -28,12 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelFond = new System.Windows.Forms.Panel();
+            this.pB2 = new System.Windows.Forms.PictureBox();
+            this.pB1 = new System.Windows.Forms.PictureBox();
+            this.panelFond.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pB2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelFond
+            // 
+            this.panelFond.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.panelFond.Controls.Add(this.pB2);
+            this.panelFond.Controls.Add(this.pB1);
+            this.panelFond.Location = new System.Drawing.Point(-2, -2);
+            this.panelFond.Name = "panelFond";
+            this.panelFond.Size = new System.Drawing.Size(452, 484);
+            this.panelFond.TabIndex = 0;
+            // 
+            // pB2
+            // 
+            this.pB2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pB2.Location = new System.Drawing.Point(125, 355);
+            this.pB2.Name = "pB2";
+            this.pB2.Size = new System.Drawing.Size(34, 34);
+            this.pB2.TabIndex = 1;
+            this.pB2.TabStop = false;
+            // 
+            // pB1
+            // 
+            this.pB1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pB1.Location = new System.Drawing.Point(300, 355);
+            this.pB1.Name = "pB1";
+            this.pB1.Size = new System.Drawing.Size(34, 34);
+            this.pB1.TabIndex = 0;
+            this.pB1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 478);
+            this.Controls.Add(this.panelFond);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panelFond.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pB2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelFond;
+        private System.Windows.Forms.PictureBox pB2;
+        private System.Windows.Forms.PictureBox pB1;
     }
 }
 
--- projetInfo/projetInfo/Form1.cs
+++ projetInfo/projetInfo/Form1.cs
@@ -11,9 +11,35 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+
+        Keys direction;
         public Form1()
         {
             InitializeComponent();
         }
+
+
+        private void Move()
+        {
+            Point p = new Point();
+            p = pB1.Location;
+
+            switch (direction)
+            {
+                case Keys.Left:
+                    p.X -= pB1.Height;
+                    break;
+                default: break;
+            }
+
+            pB1.Location = p;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            direction = e.KeyData;
+            Move();
+        }
+        
     }
 }
